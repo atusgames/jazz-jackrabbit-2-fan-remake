2afd3553e86488996fd1ba9b3381a824a30eaff3
maxogod <maxi18wars@gmail.com>
2024-07-11 01:28:47 -0300

    Placeholder for navbar

--- src/shared/navbar.tsx
+++ src/shared/navbar.tsx
@@ -1,7 +1,10 @@
 
 function NavBar() {
     return (
-        <p>Navbar</p>
+        <nav className="bg-black opacity-50 h-10 w-full flex px-2 gap-10">
+            <button className="text-white text-base">Jazz Jackrabbit 2 - Remake</button>
+            <button className="text-white text-base">Team</button>
+        </nav>
     )
 }
 
