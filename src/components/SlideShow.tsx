aa90023e90a255677732fff35cf603dcdcdb598b
maxogod <maxi18wars@gmail.com>
2024-07-12 17:20:21 -0300

    SlideShow DONE: 	- New images and scroll behavior when navigating them

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -7,6 +7,10 @@ import map from '../assets/map.png';
 import match from '../assets/match.jpg';
 import select from '../assets/select.gif';
 import ingame from '../assets/ingame.png';
+import match_castle from '../assets/match_castle.png';
+import match_electric from '../assets/match_electric.png';
+import editor_1 from '../assets/editor_1.png';
+import editor_2 from '../assets/editor_2.png';
 
 import YoutubeVid from './YoutubeVid';
 
@@ -18,6 +22,10 @@ function SlideShow() {
         <img className="w-full h-full object-contain" alt="Gameplay Image" src={match} />,
         <img className="w-full h-full object-contain" alt="Gameplay Image" src={select} />,
         <img className="w-full h-full object-contain" alt="Gameplay Image" src={ingame} />,
+        <img className="w-full h-full object-contain" alt="Gameplay Image" src={match_castle} />,
+        <img className="w-full h-full object-contain" alt="Gameplay Image" src={match_electric} />,
+        <img className="w-full h-full object-contain" alt="Gameplay Image" src={editor_1} />,
+        <img className="w-full h-full object-contain" alt="Gameplay Image" src={editor_2} />,
     ];
 
     const previews = [
@@ -26,14 +34,26 @@ function SlideShow() {
         match,
         select,
         ingame,
+        match_castle,
+        match_electric,
+        editor_1,
+        editor_2,
     ]
 
-    const nextSlide = () => {
+    const nextSlide = (id: string) => {
         setSlideIndex((slideIndex + 1) % slides.length);
+
+        // Scroll to the selected element
+        const element = document.getElementById(id);
+        element?.scrollIntoView({ behavior: 'smooth', inline: 'center' });
     };
 
-    const prevSlide = () => {
+    const prevSlide = (id: string) => {
         setSlideIndex((slideIndex - 1 + slides.length) % slides.length);
+
+        // Scroll to the selected element
+        const element = document.getElementById(id);
+        element?.scrollIntoView({ behavior: 'smooth', inline: 'center' });
     }
 
     return (
@@ -42,9 +62,9 @@ function SlideShow() {
             {slides[slideIndex]}
 
             {/* Previews */}
-            <div className="bg-slate-800 bg-opacity-35 overflow-x-scroll py-3 px-5 h-20 flex justify-center items-center gap-5">
+            <div className="bg-slate-800 bg-opacity-35 overflow-x-scroll w-full py-3 px-5 h-20 flex gap-5">
                 {previews.map((_, index) => (
-                    <button key={index} className={`aspect-video h-full bg-opacity-20 hover:brightness-110 ${index === slideIndex ? 'bg-blue-600 scale-110' : 'bg-blue-300'}`} onClick={() => setSlideIndex(index)}>
+                    <button id={`${index}`} key={index} className={`aspect-video h-full bg-opacity-20 hover:brightness-110 ${index === slideIndex ? 'bg-blue-600 scale-110' : 'bg-blue-300'}`} onClick={() => setSlideIndex(index)}>
                         <img className='w-full h-full object-contain' src={previews[index]} />
                     </button>
                 ))}
@@ -52,10 +72,10 @@ function SlideShow() {
 
             {/* Navigation */}
             <div className="flex justify-center items-center gap-5 py-1 bg-slate-800 bg-opacity-35 rounded-b-xl text-blue-500">
-                <button className='hover:brightness-150 duration-150' onClick={prevSlide}>
+                <button className='hover:brightness-150 duration-150' onClick={() => prevSlide(`${slideIndex}`)}>
                     <IoMdArrowDropleftCircle />
                 </button>
-                <button className='hover:brightness-150 duration-150' onClick={nextSlide}>
+                <button className='hover:brightness-150 duration-150' onClick={() => nextSlide(`${slideIndex}`)}>
                     <IoMdArrowDroprightCircle />
                 </button>
             </div>
