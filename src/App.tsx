31bb0606a28711ec135582d6a4667397eedb86b6
maxogod <maxi18wars@gmail.com>
2024-07-13 00:28:13 -0300

    Adding hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-// import { useState } from 'react'
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom"
 
 import JazzJackrabbit from "./components/JazzJackrabbit"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/jazz-jackrabbit-2-remake" />} />
         <Route path="/jazz-jackrabbit-2-remake" element={<JazzJackrabbit />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
