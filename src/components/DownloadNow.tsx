e6134d1cdf03a3adfb8aca3d950c99f8b13d98b5
maxogod <maxi18wars@gmail.com>
2024-07-12 21:04:12 -0300

    Change english to spanish `Download -> Descarga`

--- src/components/DownloadNow.tsx
+++ src/components/DownloadNow.tsx
@@ -5,7 +5,7 @@ function DownloadNow() {
     return (
         <>
             <div className="w-full h-20 mt-14 p-5 bg-slate-600 rounded-sm relative">
-                <h2 className="text-white">Download Jazz Jackrabbit 2: Battle Royale - Fan Remake</h2>
+                <h2 className="text-white">Descarga Jazz Jackrabbit 2: Battle Royale - Fan Remake</h2>
                 <FaLinux className="absolute right-4 top-4 text-white text-xl" />
                 <a
                     target="_blank"
@@ -15,7 +15,7 @@ function DownloadNow() {
 
             {/* Buy original */}
             <div className="w-full h-20 mt-14 p-5 bg-slate-600 rounded-sm relative">
-                <h2 className="text-white">Buy Jazz Jackrabbit 2 Collection (original)</h2>
+                <h2 className="text-white">Compra Jazz Jackrabbit 2 Collection (original)</h2>
                 <FaWindows className="absolute right-4 top-4 text-white text-xl" />
                 <a target="_blank"
                     href="https://www.gog.com/en/game/jazz_jackrabbit_2_collection"
